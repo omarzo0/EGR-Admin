0a2da6dac1b7a50bd9e50d92610f1e8451f40d05
omar_khaled_72 <omarkhaled202080@gmail.com>
2025-03-13 13:53:24 +0200

    first publish

---
+++ src/routes/index.js
@@ -0,0 +1,48 @@
+// All components mapping with path for internal routes
+
+import { lazy } from "react";
+
+const Dashboard = lazy(() => import("../pages/protected/Dashboard"));
+const Page404 = lazy(() => import("../pages/protected/404"));
+const Blank = lazy(() => import("../pages/protected/Blank"));
+const Leads = lazy(() => import("../pages/protected/Leads"));
+const Team = lazy(() => import("../pages/protected/Team"));
+const Bills = lazy(() => import("../pages/protected/Bills"));
+const ProfileSettings = lazy(() =>
+  import("../pages/protected/ProfileSettings")
+);
+
+const routes = [
+  {
+    path: "/dashboard", // the url
+    component: Dashboard, // view rendered
+  },
+
+  {
+    path: "/leads",
+    component: Leads,
+  },
+  {
+    path: "/settings-team",
+    component: Team,
+  },
+  {
+    path: "/settings-profile",
+    component: ProfileSettings,
+  },
+  {
+    path: "/settings-billing",
+    component: Bills,
+  },
+
+  {
+    path: "/404",
+    component: Page404,
+  },
+  {
+    path: "/blank",
+    component: Blank,
+  },
+];
+
+export default routes;
